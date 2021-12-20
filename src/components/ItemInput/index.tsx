ceb4577fca2cfb49c54da28c3c6fa96323a72e97
Mike <veldsinkie.mv@gmail.com>
2021-12-20 09:27:27 +0100

    feat(iteminput): typing changes

--- src/components/ItemInput/index.tsx
+++ src/components/ItemInput/index.tsx
@@ -1,8 +1,8 @@
-import { Input, Wrap, Tag, InputProps, useStyleConfig  } from "@chakra-ui/react";
+import { Input, Wrap, Tag, InputProps, useStyleConfig } from "@chakra-ui/react";
 import uniqueId from "lodash/uniqueId";
 import React, { useEffect, useState } from "react";
 
-interface BaseItem {
+export interface BaseItem {
   key: string;
   value: string;
   data?: Record<string, string>;
@@ -10,12 +10,12 @@ interface BaseItem {
 
 export type ItemInputProps = InputProps & {
   onItemCreate?: (value: string) => BaseItem;
-  onChange?: (items: BaseItem[]) => void;
+  onChangeItems?: (items: BaseItem[]) => void;
 };
 
-export function ItemInput({ onItemCreate, onChange, ...props }: ItemInputProps) {
+export function ItemInput({ onItemCreate, onChangeItems, ...props }: ItemInputProps) {
   const [input, setInput] = useState("");
-  const styles = useStyleConfig('Input', undefined, {
+  const styles = useStyleConfig("Input", undefined, {
     isMultiPart: true,
   });
 
@@ -23,49 +23,49 @@ export function ItemInput({ onItemCreate, onChange, ...props }: ItemInputProps) 
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if(e.key === "Enter") {
+    if (e.key === "Enter") {
       setItems([
-        ...items, 
-        onItemCreate 
-          ? onItemCreate(e.currentTarget.value) 
-          : { key: uniqueId(), value: e.currentTarget.value }
+        ...items,
+        onItemCreate
+          ? onItemCreate(e.currentTarget.value)
+          : { key: uniqueId(), value: e.currentTarget.value },
       ]);
 
       setInput("");
       return;
     }
 
-    if(e.key === "Backspace" && !input) {
+    if (e.key === "Backspace" && !input) {
       setItems(items.slice(0, items.length - 1));
     }
   };
 
   useEffect(() => {
-    onChange?.(items);
+    onChangeItems?.(items);
   }, [items]);
 
   return (
-    <Wrap 
-      __css={styles.field} 
-      height="unset" 
-      transition="0.2s all" 
-      w="full" 
-      borderRadius="md" 
-      py="2" 
+    <Wrap
+      __css={styles.field}
+      height="unset"
+      transition="0.2s all"
+      w="full"
+      borderRadius="md"
+      py="2"
       px="4"
     >
       {items.map((item) => (
         <Tag key={item.key}>{item.value}</Tag>
       ))}
-      <Input 
-        {...props} 
-        flex={1} 
-        ref={inputRef} 
-        value={input} 
-        variant="unstyled" 
-        onChange={(e) => setInput(e.target.value)} 
-        onKeyUp={handleInput} 
+      <Input
+        {...props}
+        flex={1}
+        ref={inputRef}
+        value={input}
+        variant="unstyled"
+        onChange={(e) => setInput(e.target.value)}
+        onKeyUp={handleInput}
       />
     </Wrap>
-  )
-};
+  );
+}
