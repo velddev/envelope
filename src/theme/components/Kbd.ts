f4561c2a4f6546bc9899f944725a84e5fb179eef
Mike <veldsinkie.mv@gmail.com>
2021-12-30 17:01:19 +0100

    cleanup(kbd): minor refactors to remove redundant code

--- src/theme/components/Kbd.ts
+++ src/theme/components/Kbd.ts
@@ -1,20 +1,17 @@
-import type { SystemStyleFunction } from "@chakra-ui/theme-tools";
-import { mode } from "@chakra-ui/theme-tools";
+import type { SystemStyleObject } from "@chakra-ui/theme-tools";
 
-const baseStyle: SystemStyleFunction = (props) => {
-  return {
-    borderColor: "ui.20",
-    borderRadius: "sm",
-    borderWidth: "1px",
-    fontSize: "0.8em",
-    fontWeight: "bold",
-    lineHeight: "normal",
-    px: "0.4em",
-    whiteSpace: "nowrap",
-    _active: {
-      borderBottomWidth: "1px",
-    },
-  };
+const baseStyle: SystemStyleObject = {
+  borderColor: "ui.20",
+  borderRadius: "sm",
+  borderWidth: "1px",
+  fontSize: "0.8em",
+  fontWeight: "bold",
+  lineHeight: "normal",
+  px: "0.4em",
+  whiteSpace: "nowrap",
+  _active: {
+    borderBottomWidth: "1px",
+  },
 };
 
 export default {
